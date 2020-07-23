91a64edcea5151a73e72d24522e2f9598bc4b51d
realeve <realeve@qq.com>
2020-07-23 14:23:32 +0800

    细节调整

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,29 @@
-'use strict';
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-gulp.task('styles', function () {
-  return gulp.src('example/styles/main.css')
-    .pipe(gulp.dest('.tmp/styles'));
+"use strict";
+var gulp = require("gulp");
+var uglify = require("gulp-uglify");
+gulp.task("styles", function () {
+  return gulp.src("example/styles/main.css").pipe(gulp.dest(".tmp/styles"));
 });
-gulp.task('connect', ['styles'], function () {
-  var serveStatic = require('serve-static');
-  var serveIndex = require('serve-index');
-  var app = require('connect')()
-    .use(serveStatic('.tmp'))
-    .use(serveStatic('example'))
-    .use(serveStatic('src'))
-    .use(serveIndex('example'));
+gulp.task("connect", ["styles"], function () {
+  var serveStatic = require("serve-static");
+  var serveIndex = require("serve-index");
+  var app = require("connect")()
+    .use(serveStatic(".tmp"))
+    .use(serveStatic("example"))
+    .use(serveStatic("src"))
+    .use(serveIndex("example"));
 
-  require('http').createServer(app)
+  require("http")
+    .createServer(app)
     .listen(3001)
-    .on('listening', function () {
-      console.log('Started connect web server on http://localhost:9000');
+    .on("listening", function () {
+      console.log("Started connect web server on http://localhost:9000");
     });
 });
 
-
-gulp.task('serve', ['connect'], function () {
-  require('opn')('http://localhost:3001');
+gulp.task("serve", ["connect"], function () {
+  require("opn")("http://localhost:3001");
 });
-gulp.task('dist', function() {
-  return gulp.src('src/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('dist'));
+gulp.task("dist", function () {
+  return gulp.src("src/*.js").pipe(uglify()).pipe(gulp.dest("js"));
 });
